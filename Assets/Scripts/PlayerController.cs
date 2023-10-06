06d8960131a3bc941668bb01cffd28be37763a22
oscarnt4 <ojep@live.co.uk>
2023-10-06 10:10:31 +0100

    Pickup functionality added
    Added trigger condition code for pickups. When pickup is collected the game object is deactivated and the pickup count is increased.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    void Start()
+    {
+        count  0;
+    }
+
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
@@ -17,5 +24,14 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive("false");
+            count++;
+        }
     }
 }
